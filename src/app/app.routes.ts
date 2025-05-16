be67230b934bbdf1ca17bc1225fc369c989e14d3
otolol <otololua@gmail.com>
2025-05-16 12:39:06 +0400

    add multiple routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { TaskListComponent } from "./task-list/task-list.component";
+import { NoTaskComponent } from "./no-task/no-task.component";
 
 
 export const routes: Routes = [
+  {
+    path: '', // <domain>
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks', //<domain>/tasks
     component: TaskListComponent
---
+++ src/app/no-task/no-task.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-no-task',
+  standalone: true,
+  template: 'Select a user to see their tasks'
+})
+export class NoTaskComponent {}
