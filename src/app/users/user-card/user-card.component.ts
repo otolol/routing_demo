e043d28b1d829455703c04b1947f2a311c27645e
otolol <otololua@gmail.com>
2025-05-16 17:17:02 +0400

    add router link.

--- src/app/users/user-card/user-card.component.ts
+++ src/app/users/user-card/user-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 export interface User {
   id: number;
@@ -12,7 +13,7 @@ export interface User {
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css']
 })
